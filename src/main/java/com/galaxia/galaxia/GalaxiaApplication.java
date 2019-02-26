675e18ce705cc04ed2adf15c59715cec0ccad444
Agustin Servat <agustinservat@gmail.com>
2019-02-26 01:22:50 -0300

    Planet and point

--- src/main/java/com/galaxia/galaxia/GalaxiaApplication.java
+++ src/main/java/com/galaxia/galaxia/GalaxiaApplication.java
@@ -1,5 +1,7 @@
 package com.galaxia.galaxia;
 
+import com.galaxia.galaxia.models.Planeta;
+import com.galaxia.galaxia.models.Point;
 import javafx.application.Application;
 import org.springframework.boot.SpringApplication;
 import org.springframework.boot.autoconfigure.SpringBootApplication;
@@ -15,7 +17,17 @@ public class GalaxiaApplication extends SpringBootServletInitializer {
 	}
 
 	public static void main(String[] args) {
+
 		SpringApplication.run(GalaxiaApplication.class, args);
+
+		Planeta Ferengi = new Planeta("Ferengi", 500, 1, true);
+		Planeta Vulcano = new Planeta("Vulcano", 1000, 5, false);
+
+		Point PositionFerengi = Ferengi.getPoint(5);
+		Point PositionVulcano = Vulcano.getPoint(5);
+
+		System.out.println("punto x es: " + PositionFerengi.getX() + " punto y es: " + PositionFerengi.getY());
+		System.out.println("punto x es: " + PositionVulcano.getX() + " punto y es: " + PositionVulcano.getY());
 	}
 
 
---
+++ src/main/java/com/galaxia/galaxia/models/Planeta.java
@@ -0,0 +1,65 @@
+package com.galaxia.galaxia.models;
+
+import static java.lang.Math.*;
+
+public class Planeta {
+
+    private String name;
+    private Integer distance;
+    private Integer degreesPerDay;
+    private boolean clockwise;
+
+    public Planeta(String name, Integer distance, Integer degreesPerDay, boolean clockwise) {
+        this.name = name;
+        this.distance = distance;
+        this.degreesPerDay = degreesPerDay;
+        this.clockwise = clockwise;
+    }
+
+    public String getName() {
+        return name;
+    }
+
+    public void setName(String name) {
+        this.name = name;
+    }
+
+    public Integer getDistance() {
+        return distance;
+    }
+
+    public void setDistance(Integer distance) {
+        this.distance = distance;
+    }
+
+    public Integer getDegrees() {
+        return degreesPerDay;
+    }
+
+    public void setDegrees(Integer degreesPerDay) {
+        this.degreesPerDay = degreesPerDay;
+    }
+
+    public boolean isClockwise() {
+        return clockwise;
+    }
+
+    public void setClockwise(boolean clockwise) {
+        this.clockwise = clockwise;
+    }
+
+    //Con coordenadas polares calculo la posicion del planeta
+    public Point getPoint(Integer days){
+        //Calculo los grados con modulo 360 para quitar las vueltas y quedarme con los grados entre 0 y 360
+        double degrees = (this.degreesPerDay * days) % 360;
+
+        if(clockwise){
+            degrees = degrees * (-1);
+        }
+        double x = this.distance * cos(toRadians(degrees));
+        double y = this.distance * sin(toRadians(degrees));
+
+        return new Point(x,y);
+    }
+
+}
