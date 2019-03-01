982df4b93132fb525ae2448ee91305d153eef547
Agustin Servat <agustinservat@gmail.com>
2019-03-01 01:00:29 -0300

    Basic issues finished

--- src/main/java/com/galaxia/galaxia/GalaxiaApplication.java
+++ src/main/java/com/galaxia/galaxia/GalaxiaApplication.java
@@ -10,6 +10,7 @@ import org.springframework.boot.web.servlet.support.SpringBootServletInitializer
 
 import java.time.LocalDate;
 import java.util.ArrayList;
+import java.util.HashMap;
 import java.util.List;
 
 @SpringBootApplication
@@ -34,39 +35,48 @@ public class GalaxiaApplication extends SpringBootServletInitializer {
 		planets.add(Betasoide);
 		SolarSystem solarSystem = new SolarSystem(planets);
 
-		Point PositionFerengi = Ferengi.getPoint(1);
-		Point PositionBetasoide = Betasoide.getPoint(1);
-		Point PositionVulcano = Vulcano.getPoint(1);
-
-		System.out.println("El punto es: " + PositionFerengi.getX() + " y el otro es: " + PositionFerengi.getY());
-		System.out.println("El punto es: " + PositionVulcano.getX() + " y el otro es: " + PositionVulcano.getY());
-		System.out.println("El punto es: " + PositionBetasoide.getX() + " y el otro es: " + PositionBetasoide.getY());
+//		Point PositionFerengi = Ferengi.getPoint(1);
+//		Point PositionBetasoide = Betasoide.getPoint(1);
+//		Point PositionVulcano = Vulcano.getPoint(1);
+//
+//		System.out.println("El punto es: " + PositionFerengi.getX() + " y el otro es: " + PositionFerengi.getY());
+//		System.out.println("El punto es: " + PositionVulcano.getX() + " y el otro es: " + PositionVulcano.getY());
+//		System.out.println("El punto es: " + PositionBetasoide.getX() + " y el otro es: " + PositionBetasoide.getY());
 
 //		Point punto = new Point(0,0);
-//		Point p1 = new Point(1,0);
-//		Point p2 = new Point(0,1);
-//		Point p3 = new Point(-1,1);
+//		Point p1 = new Point(1,1);
+//		Point p2 = new Point(2,2);
+//		Point p3 = new Point(3,3);
 //
-//		System.out.println("Esta adentro del triangulo si o no: " + solarSystem.pointInTriangle(punto, PositionFerengi, PositionBetasoide, PositionVulcano));
+//		System.out.println("Es linea o no: " + solarSystem.isLine(p1, p2, p3));
 
-//		System.out.println("El clima esta: " + solarSystem.getWeather(24));
-//
+
 		LocalDate start = LocalDate.of(2015, 9, 1);
-		LocalDate end = LocalDate.of(2105, 9, 4);
+		LocalDate end = LocalDate.of(2025, 9, 4);
 		LocalDate currentStart=LocalDate.from(start);
 		LocalDate currentEnd=LocalDate.from(end.plusDays(1));//end is inclusive
 		Integer count = 0;
-		do{
+        HashMap<String, Integer> program = new HashMap<String, Integer>();
+
+        do{
 			// do what you want with currentStart
 			//....
 			count += 1;
 			String result = solarSystem.getWeather(count);
+
+            int repeat = program.containsKey(result) ? program.get(result) : 0;
+            program.put(result, repeat + 1);
+
 //			System.out.println("Imprimio: " + count);
 //			System.out.println("El clima esta: " + solarSystem.getWeather(count));
 //			System.out.println("El perimetro es: " + SolarSystem.getPerimeter());
 //			System.out.println("-------------------------------------- ");
 			currentStart=currentStart.plusDays(1);
 		}while (!currentStart.equals(currentEnd));
+
+
+        program.forEach((k,v) -> System.out.println("Key: " + k + ": Value: " + v));
+
 
         System.out.println("El perimetro es: " + SolarSystem.getPerimeter());
 	}
